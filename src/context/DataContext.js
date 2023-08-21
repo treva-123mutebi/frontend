46716ad8d8e9c43c88a7eb31a81763fe0a50dc64
BAHINDI EMMANUEL <bahindiemmanuel20@gmail.com>
2023-08-21 22:17:29 +0300

    Updated the DataContext api in src

---
+++ src/context/DataContext.js
@@ -0,0 +1,43 @@
+import React, { createContext, useContext } from "react";
+import axios from "axios";
+
+const DataContext = createContext();
+
+export const DataContextProvider = ({ children }) => {
+  const [searchText, setSearchText] = useState("");
+
+//   const history = useHistory();
+
+  const handleSearch = async () => {
+    try {
+      const response = await axios.get(
+        `http://localhost:3000/api/track/${searchText}`
+      );
+      const searchResults = response.data;
+      console.log(searchResults);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  
+  // fetchData fetches data from the fakestoreapi
+  const fetchData = async () => {
+    try {
+      const response = await axios.get(
+        "https://fakestoreapi.com/products?limit=4"
+      );
+      //   console.log(response);
+      return response.data;
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <DataContext.Provider value={{ fetchData, handleSearch }}>
+      {children}
+    </DataContext.Provider>
+  );
+};
+
+export const useDataContext = () => useContext(DataContext);
